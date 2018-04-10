335573eb9221f0fe7844b9d2b7f5070a5cb32bad
vcday <vcday@umich.edu>
2018-04-09 21:02:14 -0400

    add professor links

--- Crawler.py
+++ Crawler.py
@@ -6,35 +6,21 @@ import Queue
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
-from selenium.webdriver import ActionChains
 
 
-def crawl(rate_my_professor, source):
-
-    driver = webdriver.Chrome()
-    driver.get(source)
-
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
-
-    professor_urls = Set()
-
-    for href in soup.find_all('a', href=True):
-        link = href.get('href')
-        if "/ShowRatings.jsp?tid=" in link:
-            link = rate_my_professor + link
-            professor_urls.add(link)
-
-    return professor_urls
-
-
-def load_more(source):
+# Click the "Load more" button max_prof times
+# Write html to file
+# Need to load javascript dynamically in order to load professor links
+#
+# source = source url with All Professors
+# max_profs = # of times to click "Load More"
+def load_more(source, max_profs):
 
     load_more_xpath = '//*[@id="mainContent"]/div[1]/div/div[5]/div/div[1]'
 
     driver = webdriver.Chrome()
     driver.get(source)
-    for x in range(20):
+    for x in range(max_profs):
         try:
             load_more_button = driver.find_element_by_xpath(load_more_xpath)
             driver.execute_script("arguments[0].scrollIntoView(true);", load_more_button)
@@ -53,6 +39,30 @@ def load_more(source):
     driver.quit()
 
 
+# "Crawl" the html file outputted from load_more
+# Find all the unique professor page links
+#
+# rate_my_professor = source link to append to local links
+def crawl(rate_my_professor):
+
+    f = open("ratemyprofessor.html", "r")
+    html = f.read()
+    soup = BeautifulSoup(html, "html.parser")
+    f.close()
+    professor_urls = Set()
+
+    for href in soup.find_all('a', href=True):
+        link = href.get('href')
+        if "/ShowRatings.jsp?tid=" in link:
+            link = rate_my_professor + link
+            professor_urls.add(link)
+            print link
+
+    return professor_urls
+
+
+# Outputs the links in the set to a file
+# set = set of links to write
 def output_to_file(set):
     f = open("professor_urls.txt", "w")
     for link in set:
@@ -65,8 +75,9 @@ if __name__ == "__main__":
 
     rate_my_professor = "http://www.ratemyprofessors.com/"
     source = 'http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=University+of+Michigan&schoolID=1258&queryoption=TEACHER'
-    #
-    # professor_urls = crawl(rate_my_professor, source)
-    # output_to_file(professor_urls)
 
-    load_more(source)
+    # load_more(source, 20)
+    professor_urls = crawl(rate_my_professor)
+    output_to_file(professor_urls)
+
+
