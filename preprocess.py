49c9713bb4c45dc63386a806af6cd5f58e8872dc
amyzhou <amyzhou@umich.edu>
2018-04-18 17:36:40 -0400

    add comments, clean up code

--- preprocess.py
+++ preprocess.py
@@ -1,12 +1,5 @@
 import json
 
-# r = {'is_claimed': 'True', 'rating': 3.5}
-# r = json.dumps(r)
-# loaded_r = json.loads(r)
-# loaded_r['rating'] #Output 3.5
-# type(r) #Output str
-# type(loaded_r) #Output dict
-
 # splits string into list of words
 def tokenizeText(s):
 
@@ -25,6 +18,7 @@ def tokenizeText(s):
 			if token[-1] in nontokens:
 				finalListOfTokens.append(token[0:len(token)-1])
 
+			# case 2: handle contractions (she's, I'm, we'd)
 			elif len(token) >=3 and token[-2] == "'":
 
 				if token[-1] == "s":
@@ -60,10 +54,11 @@ def tokenizeText(s):
 	return finalListOfTokens
 
 # fxn that removes stopwords
-def removeStopwords(listOfTokens):
+def removeStopwords(listOfTokens, extremeDict):
 
 	stopwordsstr = """
 	a
+	about
 	all
 	an
 	and
@@ -75,9 +70,12 @@ def removeStopwords(listOfTokens):
 	been
 	but
 	by 
+	do
 	few
 	from
 	for
+	get
+	had
 	have
 	here
 	how
@@ -95,7 +93,9 @@ def removeStopwords(listOfTokens):
 	on 
 	or
 	our
+	so
 	some
+	take
 	the
 	their
 	them
@@ -116,32 +116,79 @@ def removeStopwords(listOfTokens):
 	with
 	you
 	your
+	class
+	great
+	very
+	really
+	students
+	exams
+	lot
+	material
+	lecture
+	lectures
+	just
+	would
+	can
+	were
+	one
+	office
+	hours
+	has
+	also
+	well
+	much
+	time
+	makes
+	make
+	course
+	because
+	not
+	than
+	too
+	ever
+	like
+	more
+	out
+	go
 	"""
 
-	femalestr = ["her", "she"]
-	malestr = ["he", "him", "his"]
+	femalestr = ["her", "she", "herself", "girl", "woman", "lady"]
+	malestr = ["he", "him", "his", "himself", "guy", "man"]
 	stopwords = stopwordsstr.split()
 	finalListOfTokens = list()
 	genderTest = {"male": 0, "female": 0}
 
 	gender = -1
-
+	index = 1
 	for token in listOfTokens:
 
-		if token not in (stopwords and femalestr and malestr):
-			finalListOfTokens.append(token)
+		if token not in stopwords:
+			if token not in femalestr:
+				if token not in malestr:
+					
+					finalListOfTokens.append(token)
+
+					# for terms after "extremely"
+					if token == "extremely":
+						extremeWord = listOfTokens[index]
+						if extremeWord in extremeDict:
+							extremeDict[extremeWord] += 1
+						else:
+							extremeDict[extremeWord] = 1
 
 		if token in femalestr:
 			genderTest["female"] += 1
 		elif token in malestr:
 			genderTest["male"] += 1
 
+		index += 1
+
 	if genderTest["female"] > genderTest["male"]:
 		gender = 1
 	else:
 		gender = 0
 
-	return finalListOfTokens, gender
+	return finalListOfTokens, gender, extremeDict
 
 def tags(x):
 	return {
@@ -171,6 +218,9 @@ def tags(x):
 def getProfTerms(data):
 	profs = {}
 	count = 0
+	femaleTerms = {}
+	maleTerms = {}
+	extremeDict = {}
 
 	for professor in data:
 
@@ -179,8 +229,9 @@ def getProfTerms(data):
 		for comment in data[professor]["comments"]:
 			comments += comment
 
+		
 		comments = tokenizeText(comments)
-		comments, gender = removeStopwords(comments)
+		comments, gender, extremeDict = removeStopwords(comments, extremeDict)
 		uniqueWords = dict()
 
 		for tag, val in data[professor]["tags"].iteritems():
@@ -193,16 +244,52 @@ def getProfTerms(data):
 			else:
 				uniqueWords[word] = 1
 
-		prof = {"name": data[professor]["name"],
-						"gender": gender,
-						"terms": uniqueWords
-						}
+			if gender == 0:
+				if word in maleTerms:
+					maleTerms[word] += 1
+				else:
+					maleTerms[word] = 1
+			else:
+				if word in femaleTerms:
+					femaleTerms[word] += 1
+				else:
+					femaleTerms[word] = 1
 
-		profs[professor] = prof
+		if len(uniqueWords) >= 5:
+
+			prof = {"name": data[professor]["name"],
+							"gender": gender,
+							"terms": uniqueWords
+							}
+
+			profs[professor] = prof
 
 	output = json.dumps(profs, indent=4)
-	f = open("profTerms.json", "w")
+	f = open("profTerms2.json", "w")
 	f.write(output)
+
+	termFile = open("termFile.output", "w")
+	termFile.write("TOP 20 MALE TERMS:\n")
+	count = 1
+	for key, value in sorted(maleTerms.iteritems(), key=lambda (k,v): (v,k), reverse = True):
+		termFile.write("%s %s" % (key.encode('utf-8'), value) + "\n")
+		count += 1
+		if count == 21:
+			break
+	count = 1
+	termFile.write("\nTOP 20 FEMALE TERMS\n")
+	for key, value in sorted(femaleTerms.iteritems(), key=lambda (k,v): (v,k), reverse = True):
+		termFile.write("%s %s" % (key.encode('utf-8'), value) + "\n")
+		count += 1
+		if count == 21:
+			break
+	count = 1
+	termFile.write("\nTOP 20 EXTREME FEMALE TERMS\n")
+	for key, value in sorted(extremeDict.iteritems(), key=lambda (k,v): (v,k), reverse = True):
+		termFile.write("%s %s" % (key.encode('utf-8'), value) + "\n")
+		count += 1
+		if count == 21:
+			break
 
 # run if main file
 if __name__ == "__main__":
